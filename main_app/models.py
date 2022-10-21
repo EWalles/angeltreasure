e6a96614f43da68d3744c5894d1a4613d6c6cdd8
EWalles <ejomcdonald@gmail.com>
2022-10-21 10:17:58 -0400

    backend with a bit to css

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+# Create your models here
+
+class Bead(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=50)
+
+def get_absolute_url(self):
+    return reverse('bead_detail', kwargs={'pk': self.id})
+
+class Jewelry(models.Model):
+  name = models.CharField(max_length=100)
+  type = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  beads = models.ManyToManyField(Bead)
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,17 @@
+from pyexpat import model
+from django.shortcuts import render, redirect
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from main_app.models import Jewelry, Bead
+from .forms import CustomerForm
 
-from django.shortcuts import render
-from django.http import HttpResponse
-
-#home
 def home(request):
-  return HttpResponse('<h1>Angels Treasures</h1>')
+    return render(request,'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def jewelry_index(request):
+  jewelry = Jewelry.objects.all()
+  return render(request, 'jewelry/index.html', { 'jewelry': jewelry })
